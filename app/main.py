84550d0f270e070d33c0fa1c80e10249be0dd9cf
11081999 <33244698+RobertoRiveraT@users.noreply.github.com>
2025-05-16 12:48:37 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,5 +9,5 @@ app = FastAPI()
 def root():
     return {"message": "Chatbot Arelia API is up and running!"}
 
-# 👇 Esta línea crea las tablas en la base de datos (solo la necesitas una vez)
+# 👇 Esta línea crea las tablas en la base de datos (solo la necesitas una vez).
 models.Base.metadata.create_all(bind=database.engine)
