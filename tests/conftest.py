3ee4e32637b3ecbca4b70599041fd817581b761a
11081999 <33244698+RobertoRiveraT@users.noreply.github.com>
2025-06-11 17:01:55 -0600

    Chore: added a conftest.py file to share session between tests and update readme

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,11 @@ import pytest
 from fastapi.testclient import TestClient
 from app.main import app
 
+from dotenv import load_dotenv
+## Load the .env.staging file
+# load_dotenv(dotenv_path=".env.staging")
+# Now the environment is loaded before any app import
+
 @pytest.fixture(scope="session")
 def client():
     """
